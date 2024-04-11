3c67cf19fa4bea685255ad301345b5b7a04bd492
Illimar <illimar.rekand@gmail.com>
2024-04-11 22:13:22 +0200

    Add faceting, unpacking

--- scripts/2024-03-19-NKS-survey-2023-2024-shiny.R
+++ scripts/2024-03-19-NKS-survey-2023-2024-shiny.R
@@ -5,8 +5,10 @@
 library(gsheet)
 library(shiny)
 library(ggplot2)
-library(forcats) #fct
+library(forcats) #fct_infreq
 library(dplyr)
+library(tidyverse)
+library(ggtext) # fix ggtitle width
 
 ################################################################################################################
 ################################################ Extracting data ###############################################
@@ -46,7 +48,7 @@ ggplot(sheet.NA.rm, aes(x = if (scale.question==TRUE){as.character(sheet.NA.rm[[
 ################################################ Shiny #########################################################
 ################################################################################################################
 
-ui <- fluidPage(
+ui <- bootstrapPage(
   
   # App title ----
   titlePanel("Choose input below"),
@@ -66,7 +68,7 @@ ui <- fluidPage(
                   choices = c("None", names(sheet)) #Adds a none for when filling is not necessary
       ),
       checkboxInput("facet.wrap", "Facet Wrap?", value = FALSE),
-      conditionalPanel(condition = "input.facet.wrap == TRUE",
+      conditionalPanel(condition = "input.facet.wrap == true",
         numericInput('n_facet', 'Number of top facets', 4, min = 1, max = 7)
       ),
       downloadButton('downloadPlot')
@@ -89,12 +91,6 @@ ui <- fluidPage(
 server <- function(input, output) { #shiny passes selectInput as a string. To use these variables for subsetting dataframes, use e.g. df$!!sym(input$variable) or df[[input$variable]]
   
   formulaText <- reactive({
-    caption.txt <- input$variable
-    caption.txt <- gsub(pattern = "1\\.5", replacement = "1 to 5", x = caption.txt) # "." means any character. This is why we use "\\." instead
-    caption.txt <- gsub(pattern = "\\.\\.", replacement = " ", x = caption.txt) # "." means any character. This is why we use "\\." instead
-    caption.txt <- gsub(pattern = "\\.", replacement = " ", x = caption.txt) # "." means any character. This is why we use "\\." instead
-    paste0(
-      caption.txt, "?")
   })
   
   # Return the formula text for printing as a caption ----
@@ -105,26 +101,60 @@ server <- function(input, output) { #shiny passes selectInput as a string. To us
   
   # Generate a plot of the requested variable against count ----
   bar_plot.reactive <- reactive({
-    scale.question <- FALSE
+    
+    #fix caption from column title
+    caption.txt <- input$variable #spaces have been converted to ".". Below we fix this.
+    caption.txt <- gsub(pattern = "1\\.5", replacement = "1 to 5", x = caption.txt) # "." means any character. This is why we use "\\." instead
+    caption.txt <- gsub(pattern = "\\.\\.", replacement = " ", x = caption.txt) # "." means any character. This is why we use "\\." instead
+    caption.txt <- gsub(pattern = "\\.", replacement = " ", x = caption.txt) # "." means any character. This is why we use "\\." instead
+    caption.txt <- paste0(substring(caption.txt, 1, nchar(caption.txt)-1), "?") #it's nice to have a question mark at the end of a question
+    
+    #scale-question parameters
     scale.question <- grepl(x = input$variable, pattern = "scale", ignore.case = TRUE) #is this a question with a ranking from 1-5?
+    
+    
+    #unpack lists (cells with multiple values)
+    cells.w.multiple.value <- any(grepl(pattern = ",", x = sheet[[input$variable]]))
+    if (cells.w.multiple.value){
+      sheet.unpackt <- sheet %>% separate_longer_delim(!!sym(input$variable), delim = ", ")
+      sheet <- sheet.unpackt
+    }
+    
+    # remove NA
     sheet.NA.rm <- subset(sheet, !is.na(sheet[[input$variable]]))
+    #handling counts for facetted plots
     if(input$facet.wrap){
-    sheet.w.count <- sheet.NA.rm %>% group_by(!!sym(col.name.txt)) %>% add_count() #find the counts for each fill value
+    sheet.w.count <- sheet.NA.rm %>% group_by(!!sym(input$fill_value)) %>% add_count() #find the counts for each fill value
     count_values <- sort(unique(sheet.w.count$n), decreasing=TRUE) #find unique count values, sorts them high->low
 
-    sheet.top.n.subset <- subset(sheet.w.count, subset = n %in% count_values[1:input$n_facet] )
-    sheet.NA.rm <- sheet.top.n.subset
-  }
+    sheet.top.n.subset <- subset(sheet.w.count, subset = n %in% count_values[1:input$n_facet] ) # creates a subset of the dataset which contains only the top n counts (n is defined in the shiny app as n_facet)
+    sheet.NA.rm <- sheet.top.n.subset # pipelines the data
+    }
+    
+    # let's plot
     ggplot(
-      sheet.NA.rm, aes(x = if (scale.question==TRUE){as.character(sheet.NA.rm[[input$variable]])}else {fct_rev(fct_infreq(as.character(sheet.NA.rm[[input$variable]])))} ))+ #fct_infreq = order by count, fct_rev = reverse order. Scale questions should not be ordered by count, and needs to converted to character type
+      sheet.NA.rm, aes(x = if (scale.question==TRUE){as.character(sheet.NA.rm[[input$variable]])}
+                       else {fct_rev(fct_infreq(as.character(sheet.NA.rm[[input$variable]])))}
+                       ))+ #fct_infreq = order by count, fct_rev = reverse order. Scale questions should not be ordered by count, and needs to converted to character type
       geom_bar(
         if(input$fill_value == "None"){} #if fill is "None", then fill with no color
-               else {aes(fill=!!sym(input$fill_value))}
+               else {aes(fill=
+                           if(scale.question){as.character(!!sym(input$fill_value))}
+                         else{!!sym(input$fill_value)}
+                         )
+                 }
         ) +
       geom_text(stat='count', aes(label=..count..), vjust=if(scale.question==FALSE){0}else{-1}, hjust=if(scale.question==FALSE){-0.5}else{0}) +
       theme_classic() +
       {if(scale.question==FALSE)coord_flip()} + #scale question should not be coord-flipped.
-      theme(legend.title = element_blank(), axis.title.y = element_blank(), legend.position=if(input$facet.wrap){"none"}else{"right"}) +
+      theme(text = element_text(size = 24),
+            plot.title = element_textbox_simple(),
+            legend.title = element_blank(),
+            axis.title.y = element_blank(),
+            legend.position=if(input$facet.wrap){"none"}else{"right"}, 
+            strip.placement = "outside",
+            ) +
+      ggtitle(caption.txt) +
     {if(input$facet.wrap)facet_wrap(vars(sheet.NA.rm[[input$fill_value]]), ncol = 2)}
   })
   
@@ -136,9 +166,15 @@ server <- function(input, output) { #shiny passes selectInput as a string. To us
   
   output$downloadPlot <- downloadHandler(
     filename <- function()
-    {paste0("NKS-plot-",input$variable,".png")},
+    { if (input$facet.wrap){
+      paste0("NKS-plot-",input$variable,"-facet-wrap-",input$fill_value, ".png")
+    }else
+    {
+      paste0("NKS-plot-",input$variable,".png")
+    }
+      },
     content <- function(file){
-      png(file=file)
+      png(file=file, width = 1000, height = 1000, pointsize = 24) #can we make this work with ggsave? https://stackoverflow.com/questions/14810409/how-to-save-plots-that-are-made-in-a-shiny-app
       plot(bar_plot.reactive())
       dev.off()
     }
